fe479bcfcc80ce2a3d0423d6982e68b69432ab9a
Côme Willot <comewillot@MacBook-Pro-de-Come.local>
2018-05-18 18:19:23 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-import Board from './Board';
+import Board from './components/Board';
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return (
