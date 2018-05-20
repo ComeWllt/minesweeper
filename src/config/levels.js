e3ab700469664828c3fbafbe8bde30d3208b51fa
Côme Willot <comewillot@MacBook-Pro-de-Come.local>
2018-05-20 18:13:59 +0200

    levels values

--- src/config/levels.js
+++ src/config/levels.js
@@ -1,7 +1,7 @@
 
 const levels = {
   beginner: {rowNumber: 8, columnNumber: 8, bombNumber: 10},
-  intermediate: {rowNumber: 10, columnNumber: 13, bombNumber: 25},
+  intermediate: {rowNumber: 10, columnNumber: 12, bombNumber: 20},
   expert: {rowNumber: 12, columnNumber: 20, bombNumber: 45}
 };
 
